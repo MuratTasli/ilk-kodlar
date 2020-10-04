4538f7fa7a8391c3258acba9ca416666a1bcfb0a
MuratTasli <71225768+MuratTasli@users.noreply.github.com>
2020-10-04 19:53:15 +0300

    500'e kadar tüm dik üçgenleri bulma
    Bu program dik kenarları ve hipotenüsü 500'den büyük olmayan tüm dik üçgenleri ekrana basar.

---
+++ hipotenüs_app.c
@@ -0,0 +1,21 @@
+/*KENAR1, KENAR2 YADA HÝPOTENÜSÜ 500 DEN BÜYÜK OLMAYAN TÜM DÝK ÜÇGENLERÝ BULMA*/
+#include<stdio.h>
+
+int main(){
+
+int side1, side2,hypotenuse;
+
+for(side1=1;side1<=500;side1++){
+	
+	for(side2=1;side2<=500;side2++){
+		hypotenuse=(pow(side1,2)+pow(side2,2));
+		if(hypotenuse<=500)
+			printf("%d\t%d\t%d\n",side1,side2,hypotenuse);
+		
+		else
+			break;
+	}
+}
+
+return 0;
+}
